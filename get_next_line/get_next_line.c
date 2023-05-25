74805ae09f2c27b17d7010c94dd7bb7a640c492a
Anais Gassin <angassin@student.s19.be>
2023-05-25 12:16:41 +0200

    exam02 removed main from gnl.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -8,17 +8,17 @@ int		ft_strlen(char *s);
 void	*ft_free(void *ptr);
 
 char	*get_next_line(int fd)
-{      
-	
+{
+
 	char	buf[BUFFER_SIZE + 1];
 	char 	*line = NULL;
 	static	char *container;
 	int		status = 1;
 	char	*to_free;
-	
+
 	if (read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0 || fd > OPEN_MAX || fd >= INT_MAX)
 		return (ft_free(container));
-	
+
 	//read and append
 	*buf = '\0';
 	while ((status > 0) && !ft_strchr(buf, '\n'))
@@ -33,18 +33,18 @@ char	*get_next_line(int fd)
 			return(NULL);
 		free(to_free);
 	}
-	
+
 	//extract line
 	char	*end_of_line;
 	int		line_len;
 	line_len = 0;
-	end_of_line = ft_strchr(container, '\n'); 
+	end_of_line = ft_strchr(container, '\n');
 	if (end_of_line != NULL)
 	{
 		end_of_line++; //\n included
 		line_len = end_of_line - container;
 		line = malloc(sizeof(*line) * (line_len + 1));
-		if (!line)	
+		if (!line)
 			return(ft_free(container));
 		for (int i = 0; i < line_len; i++)
 			line[i] = container[i];
@@ -78,7 +78,7 @@ char	*gnl_strjoin(char *s1, char	*s2)
 	char	*joined;
 	int		size = 0;
 	int i = 0;
-	
+
 	if (!s1 && !s2)
 		return(NULL);
 	if (s1)
@@ -128,56 +128,3 @@ void	*ft_free(void *ptr)
 	return (NULL);
 }
 
-# include <fcntl.h>
-# include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	char	*line;
-	int		fd;
-	int 	i = 0;
-
-//	(void)argc;
-//	(void)argv;
-//	fd = 0;
-
-	if (argc < 2)
-	{
-		printf("Usage: ./<program>.out fd\n");
-		exit(1);
-	}
-
-	// check size of buffer
-	// printf ("BUFFER_SIZE = %d\n", BUFFER_SIZE);
-	
-	//open file
-	fd = open(argv[1], O_RDONLY);
-	printf("fd = %d\n\n", fd);
-/*	if (fd < 0)
-	{
-		printf("Could not open file.\n");
-		exit(1);
-	}
-*/	
-	// print each line until EOF
-	while (1)
-	{
-		i++;
-		printf("start of loop\n");
-		line = get_next_line(fd);
-		// line = get_next_line(fd);
-		if (!line)
-			break;
-		printf("line %d : %s", i, line);
-		printf("\nend of loop\n");
-		free(line);
-	}
-
-	// close file
-	if (close(fd) < 0)
-	{
-		printf("Could not close the file.\n");
-		exit(1);
-	}
-	return (0);
-}
